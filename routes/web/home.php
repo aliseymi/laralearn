a407bf917bfda42e156f484939614ae4685ad1df
seymi <seyedali1377mir@gmail.com>
2021-11-03 21:19:35 +0330

    create model and migration for commnets and implement polymorphic relationship

--- routes/web/home.php
+++ routes/web/home.php
@@ -15,11 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//    $container = new \App\Container();
-//    $container->bind('fooService', function (){
-//        return new \App\Services\FooService();
-//    });
-
     return view('welcome');
 });
 
@@ -42,4 +37,7 @@ Route::prefix('profile')->middleware('auth')->group(function (){
 
 Route::get('auth/token',[\App\Http\Controllers\Auth\AuthTokenController::class,'getToken'])->name('2fa.token');
 Route::post('auth/token',[\App\Http\Controllers\Auth\AuthTokenController::class,'postToken']);
+
+Route::get('products',[\App\Http\Controllers\ProductController::class,'index']);
+Route::get('products/{product}',[\App\Http\Controllers\ProductController::class,'single']);
 
